5784368465d98ea8deba287018a41d9131cd16d7
Cláudio Schirmer Guedes <claudioschirmer@icloud.com>
2024-02-23 08:44:47 -0500

    fix

--- build.gradle.kts
+++ build.gradle.kts
@@ -109,7 +109,7 @@ publishing {
 	repositories {
 		maven {
 			name = "GitHub"
-			url = uri("https://maven.pkg.github.com/ClaudioSchirmer/packages")
+			url = uri(repositoryURL)
 			credentials {
 				username = System.getenv("MAVEN_USERNAME")
 				password = System.getenv("MAVEN_PASSWORD")
